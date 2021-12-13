eccfd108a497e510c2b858f44c323a13709aad67
hammer200911 <jdaniel231@gmail.com>
2021-12-13 14:59:56 -0300

    controle de acesso

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,15 @@ import RegisterScreen from "./screens/auth/register";
 import HomeScreen from "./screens/home";
 import PetScreen from "./screens/pet/index";
 import UserEditScreen from "./screens/users/edit";
+import PrivateRoute from './components/auth/private_route'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={HomeScreen} />
       <Route exact path="/register" component={RegisterScreen} />
-      <Route exact path="/pets" component={PetScreen} />
       <Route exact path="/users/edit" component={UserEditScreen} />
+      <PrivateRoute exact path="/pets" component={PetScreen} />
     </Switch>
   </BrowserRouter>
 )
